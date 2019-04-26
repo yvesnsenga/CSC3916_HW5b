04f6ad433046a7ea8d375dacae6df92d84786783
yvesnsenga <46980788+yvesnsenga@users.noreply.github.com>
2019-04-25 23:34:25 -0600

    Comment box

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,18 +1,74 @@
 import React, { Component }  from 'react';
 import {connect} from "react-redux";
-import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
+import {Glyphicon, Panel, ListGroup, ListGroupItem, ControlLabel} from 'react-bootstrap'
 import { Image } from 'react-bootstrap'
 import { withRouter } from "react-router-dom";
 import {fetchMovie} from "../actions/movieActions";
-
+import runtimeEnv from "@mars/heroku-js-runtime-env";
+import Form from "react-bootstrap/es/Form";
+import FormGroup from "react-bootstrap/es/FormGroup";
+import Col from "react-bootstrap/es/Col";
+import FormControl from "react-bootstrap/es/FormControl";
+import Button from "react-bootstrap/es/Button";
 //support routing by creating a new component
 
 class Movie extends Component {
+    constructor(props) {
+        super(props);
+        this.updateDetails = this.updateDetails.bind(this);
+        this.commentSub = this.commentSub.bind(this);
 
+        this.state = {
+            details:{
+                review: '',
+                rating: 0
+            }
+        };
+    }
     componentDidMount() {
         const {dispatch} = this.props;
         if (this.props.selectedMovie == null)
             dispatch(fetchMovie(this.props.movieId));
+    }
+    updateDetails(event){
+        let updateDetails = Object.assign({}, this.state.details);
+
+        updateDetails[event.target.id] = event.target.value;
+        this.setState({
+            details: updateDetails
+        });
+    }
+
+
+    commentSub() {
+        const env = runtimeEnv();
+
+        var json = {
+            review: this.state.details.review,
+            rating: this.state.details.rating,
+            movie: this.props.movie
+        };
+
+        return fetch(`${env.REACT_APP_API_URL}/comments`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify(json),
+            mode: 'cors'})
+            .then( (response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then( (res) => {
+                window.location.reload();
+            })
+            .catch( (e) => console.log(e) );
+
     }
 
     render() {
@@ -31,7 +87,7 @@ class Movie extends Component {
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
             );
-        }
+        };
 
         const DetailInfo = ({currentMovie}) => {
             if (!currentMovie) { // evaluates to true if currentMovie is null
@@ -50,8 +106,43 @@ class Movie extends Component {
                 </Panel>
             );
         };
+
         return (
-            <DetailInfo currentMovie={this.props.selectedMovie} />
+            <div>
+                <DetailInfo currentMovie={this.props.selectedMovie} />
+                <Form horizontal>
+                    <FormGroup controlId="review">
+                        <Panel.Heading>Your Comment Here :</Panel.Heading>
+                        <Col componentClass={ControlLabel} sm={20}>
+                            <div className="form-group">
+                        <textarea rows="5" cols="90">
+                        </textarea>
+                            </div>
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails} value={this.state.details.review} type="text" placeholder="type review here..." />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="rating">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Rating
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails}
+                                         value={this.state.details.rating}
+                                         type="number" min="1" max="5" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button onClick={this.commentSub}>Submit</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+
+            </div>
+
         );
     }
 }
@@ -62,6 +153,6 @@ const mapStateToProps = (state, ownProps) => {
         selectedMovie: state.movie.selectedMovie,
         movieId: ownProps.match.params.movieId
     }
-}
+};
 
 export default withRouter(connect(mapStateToProps)(Movie));
